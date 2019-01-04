1649ced1c4d05ac497ea1f5c96e2a9df186e442f
tgoto63 <tgoto63@gmail.com>
2019-01-04 20:50:11 +0900

    Fix: Twitter Card validator errors.

--- src/pages/siteMetadata.tsx
+++ src/pages/siteMetadata.tsx
@@ -29,12 +29,15 @@ const SiteMetadata = ({ pathname }: any) => (
         <meta name="docsearch:version" content="2.0" />
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover" />
         <meta property="og:url" content={siteUrl} />
+        <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en" />
         <meta property="og:site_name" content={title} />
         <meta property="og:image" content={`${siteUrl}${icon}`} />
         <meta property="og:image:width" content="512" />
         <meta property="og:image:height" content="512" />
+        <meta name="twitter:text:title" content={title} />
+        <meta name="twitter:title" content={title} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content={twitter} />
       </Helmet>
